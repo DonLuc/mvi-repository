0bf9d8fb752ef414cc31d162972765b271c18b82
Lucas <maleselan@discovery.co.za>
2021-05-26 07:45:03 +0200

    chore: Remove APIService

--- app/src/main/java/com/lucas/medical_equip/MainActivity.kt
+++ app/src/main/java/com/lucas/medical_equip/MainActivity.kt
@@ -3,12 +3,11 @@ package com.lucas.medical_equip
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lucas.medical_equip.service.APIService
 import com.lucas.medical_equip.service.RetrofitBuilder
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
 import io.reactivex.rxjava3.schedulers.Schedulers
 import mu.KotlinLogging
-import java.util.*
+
 private val logger = KotlinLogging.logger {}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
