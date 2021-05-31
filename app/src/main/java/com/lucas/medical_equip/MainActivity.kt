757913dbee4dc527329d7018809ecb4dec108973
Lucas <maleselan@discovery.co.za>
2021-05-31 23:41:30 +0200

    chore: Added the search functionality

--- app/src/main/java/com/lucas/medical_equip/MainActivity.kt
+++ app/src/main/java/com/lucas/medical_equip/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.lucas.medical_equip
 
 import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Menu
+import android.widget.ProgressBar
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.lucas.medical_equip.repository.MedicalTool
@@ -16,15 +22,43 @@ class MainActivity : AppCompatActivity() {
     private lateinit var medicalTools: List<MedicalTool>
     private lateinit var medicalRecyclerView: RecyclerView
     private lateinit var medicalAdapter: MedicalToolAdapter
-
+    private lateinit var progressBar: ProgressBar
+    private lateinit var searchView: SearchView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getMedicalTools()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        progressBar = findViewById(R.id.progresssBar11)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        if (menu != null) {
+            searchView = menu.findItem(R.id.action_search).actionView as SearchView
+        }
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.maxWidth = Integer.MAX_VALUE
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                medicalAdapter.filter.filter(newText)
+                return false
+            }
+        })
+
+        return true
+    }
+
     @SuppressLint("CheckResult")
     fun getMedicalTools() {
+        //progressBar.
         val medTools = RetrofitBuilder.apiService.getMedicalEquipments()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
--- app/src/main/java/com/lucas/medical_equip/MedicalToolAdapter.kt
+++ app/src/main/java/com/lucas/medical_equip/MedicalToolAdapter.kt
@@ -4,13 +4,22 @@ import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.lucas.medical_equip.repository.MedicalTool
+import com.squareup.picasso.Picasso
+import java.util.*
+import kotlin.collections.ArrayList
 
-class MedicalToolAdapter(private val medicalTools: List<MedicalTool>) : RecyclerView.Adapter<MedicalToolAdapter.ViewHolder>() {
+class MedicalToolAdapter(private var medicalTools: List<MedicalTool>) : RecyclerView.Adapter<MedicalToolAdapter.ViewHolder>(), Filterable {
+    var medicalToolsFiltered = ArrayList<MedicalTool>()
 
+    init {
+        medicalToolsFiltered = medicalTools as ArrayList<MedicalTool>
+    }
     inner class ViewHolder(medicalToolItemView: View) : RecyclerView.ViewHolder(medicalToolItemView) {
         val medicalToolImage = itemView.findViewById<ImageView>(R.id.medical_tool_image)
         val medicalToolDescription = itemView.findViewById<TextView>(R.id.medical_tool_text)
@@ -25,17 +34,45 @@ class MedicalToolAdapter(private val medicalTools: List<MedicalTool>) : Recycler
     }
 
     override fun getItemCount(): Int {
-        return medicalTools.size
+        return medicalToolsFiltered.size
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         //Get data model based on position
-        val medicalTool: MedicalTool = medicalTools.get(position)
+        val medicalTool: MedicalTool = medicalToolsFiltered.get(position)
 
         //Set the items views based on your views data model
         val medicalToolImage = holder.medicalToolImage
         val medicalToolDescription = holder.medicalToolDescription
-        medicalToolImage.setImageURI(Uri.parse(medicalTool.imageURL))
+        //medicalToolImage.setImageURI(Uri.parse(medicalTool.imageURL))
+        Picasso.get().load(medicalTool.imageURL).into(medicalToolImage)
         medicalToolDescription.setText(medicalTool.description)
+    }
+
+    override fun getFilter(): Filter {
+        return object: Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val charString = constraint.toString()
+                if(charString.isEmpty()) {
+                    medicalToolsFiltered = medicalTools as ArrayList<MedicalTool>
+                } else {
+                    val resultList = ArrayList<MedicalTool>()
+                    for(medicalItem in medicalTools) {
+                        if(medicalItem.description.toLowerCase(Locale.ROOT).contains(charString.toLowerCase(Locale.ROOT))) {
+                            resultList.add(medicalItem)
+                        }
+                    }
+                    medicalToolsFiltered = resultList
+                }
+                val filterResults = FilterResults()
+                filterResults.values = medicalToolsFiltered
+                return filterResults
+            }
+
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                medicalToolsFiltered = results?.values as ArrayList<MedicalTool>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
